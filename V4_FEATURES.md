11bfcd3e5d05423393e6ec219778f5ff8a779c64
Eric COURTIAL <eric.courtial@wizaplace.com>
2021-09-25 09:26:35 +0200

    Added some improvements in the display of the todo and trading pages

--- V4_FEATURES.md
+++ V4_FEATURES.md
@@ -3,6 +3,7 @@
 * Front app is no longer included.
 * To watch serious and to watch background are merged.
 * True REST APIs:
-  * endpoint are reflecting resources and no longer screens ;
+  * endpoint are reflecting ressources and no longer screens ;
   * using HTTP verbs instead of different endpoints for each action.
 * Games are no longer duplicated (one entry per platform). We link them to the platforms instead.
+* We can now see if a game is completed or entirely watched. Two new filters: "Finished" and "Not finished".
--- static/js/app/tools.js
+++ static/js/app/tools.js
@@ -10,6 +10,29 @@ define(
             // Used to filter content from JS injection
             filterContent: function(content) {
                 return $('#extraP').text(content).html()
+            },
+
+            getMonthName: function(monthId) {
+                var correspondence = {
+                    1: "Janvier",
+                    2: "Février",
+                    3: "Mars",
+                    4: "Avril",
+                    5: "Mai",
+                    6: "Juin",
+                    7: "Juillet",
+                    8: "Août",
+                    9: "Septembre",
+                    10: "Octobre",
+                    11: "Novembre",
+                    12: "Décembre",
+                };
+
+                if (!(monthId in correspondence)) {
+                    return "Mois inconnu";
+                }
+
+                return correspondence[monthId];
             }
         };
     }
