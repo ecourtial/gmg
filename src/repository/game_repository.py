ec55bb80b237a2e0cb282f19b6d57ee79e761eed
Eric COURTIAL <eric.courtial@wizacha.com>
2022-05-27 15:24:01 +0200

    wip3

--- src/repository/game_repository.py
+++ src/repository/game_repository.py
@@ -2,18 +2,19 @@
 from itertools import count
 from src.repository.abstract_repository import AbstractRepository
 from src.entity.game import Game
+from src.entity.version import Version
 
 class GameRepository(AbstractRepository):
-    def get_by_id(self, platform_id):
-        """Get one support by its id."""
-        request = "SELECT * FROM games "
-        request += "WHERE id = %s LIMIT 1;"
+    def get_by_id(self, game_id):
+        """Get one game by its id."""
+        request = f"SELECT * FROM {Game.table_name} "
+        request += f"WHERE {Game.primary_key} = %s LIMIT 1;"
 
-        return self.fetch_one(request, (platform_id,))
+        return self.fetch_one(request, (game_id,))
 
     def get_by_title(self, title):
         """Get one support by its title."""
-        request = "SELECT * FROM games "
+        request = f"SELECT * FROM {Game.table_name} "
         request += "WHERE title = %s LIMIT 1;"
 
         return self.fetch_one(request, (title,))
@@ -29,12 +30,12 @@ class GameRepository(AbstractRepository):
         return self.get_by_id(game.get_id())
 
     def get_versions_count_for_game(self, game_id):
-        request = "SELECT COUNT(*) as count FROM versions WHERE game_id = %s"
+        request = f"SELECT COUNT(*) as count FROM {Version.table_name} WHERE game_id = %s"
         
         return self.fetch_cursor(request, (game_id,))
     
     def delete(self, game_id):
-        request = "DELETE FROM games WHERE id = %s"
+        request = f"DELETE FROM {Game.table_name} WHERE id = %s"
         self.write(request, (game_id,), True)
 
     def get_list(self, page, limit):
--- src/repository/platform_repository.py
+++ src/repository/platform_repository.py
@@ -2,19 +2,20 @@
 from itertools import count
 from src.repository.abstract_repository import AbstractRepository
 from src.entity.platform import Platform
+from src.entity.version import Version
 
 class PlatformRepository(AbstractRepository):
     def get_by_id(self, platform_id):
         """Get one support by its id."""
-        request = "SELECT id, name FROM platforms "
-        request += "WHERE id = %s LIMIT 1;"
+        request = f"SELECT * FROM {Platform.table_name} "
+        request += f"WHERE {Platform.primary_key} = %s LIMIT 1;"
 
         return self.fetch_one(request, (platform_id,))
 
     def get_by_name(self, name):
         """Get one support by its name."""
-        request = "SELECT id, name FROM platforms "
-        request += "WHERE name = %s LIMIT 1;"
+        request = f"SELECT * FROM {Platform.table_name} "
+        request += f"WHERE name = %s LIMIT 1;"
 
         return self.fetch_one(request, (name,))
 
@@ -29,12 +30,12 @@ class PlatformRepository(AbstractRepository):
         return self.get_by_id(platform.get_id())
 
     def get_versions_count_for_platform(self, platform_id):
-        request = "SELECT COUNT(*) as count FROM versions WHERE platform_id = %s"
+        request = f"SELECT COUNT(*) as count FROM {Version.table_name} WHERE platform_id = %s"
         
         return self.fetch_cursor(request, (platform_id,))
     
     def delete(self, platform_id):
-        request = "DELETE FROM platforms WHERE id = %s"
+        request = f"DELETE FROM {Platform.table_name} WHERE id = %s"
         self.write(request, (platform_id,), True)
 
     def get_list(self, page, limit):
