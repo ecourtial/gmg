639cfbe787d96485cf9817ba93d0b92823399589
Eric COURTIAL <57905107+ecourtial@users.noreply.github.com>
2024-06-08 19:18:22 +0200

    (fix) Update 4.4.0.sql

--- migrations/4.4.0.sql
+++ migrations/4.4.0.sql
@@ -5,5 +5,5 @@ CREATE TABLE `notes` (
   `title` varchar(255) NOT NULL,
   `content` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
