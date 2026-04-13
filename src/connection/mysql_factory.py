297b1033f9b51eed4d562cb5f3fa43b6829ece4c
Eric COURTIAL <e.courtial30@gmail.com>
2026-04-13 11:25:39 +0200

    Improve MysQL factory and usage

--- src/connection/mysql_factory.py
+++ src/connection/mysql_factory.py
@@ -1,33 +1,39 @@
+import threading
 from typing import Any
 
 from mysql import connector
 
 
 class MySQLFactory:
+    _local: threading.local = threading.local()
 
     @classmethod
     def init(cls, host: str, user: str, password: str, database: str) -> None:
-        cls.host = host
-        cls.user = user
-        cls.password = password
-        cls.database = database
-        cls.connection = None
+        cls._host = host
+        cls._user = user
+        cls._password = password
+        cls._database = database
 
     @classmethod
     def get(cls) -> Any:
-        """Get a connection"""
-        if cls.connection is None:
-            cls.connection = connector.connect(
-                host=cls.host,
-                user=cls.user,
-                passwd=cls.password,
-                database=cls.database
+        """Get a thread-local connection, creating or reconnecting if needed."""
+        conn = getattr(cls._local, 'connection', None)
+        if conn is None:
+            conn = connector.connect(
+                host=cls._host,
+                user=cls._user,
+                passwd=cls._password,
+                database=cls._database
             )
+            cls._local.connection = conn
+        else:
+            conn.ping(reconnect=True)
 
-        return cls.connection
+        return conn
 
     @classmethod
     def close(cls) -> None:
-        if cls.connection is not None:
-            cls.connection.close()
-            cls.connection = None
+        conn = getattr(cls._local, 'connection', None)
+        if conn is not None:
+            conn.close()
+            cls._local.connection = None
--- src/repository/abstract_core_repository.py
+++ src/repository/abstract_core_repository.py
@@ -9,30 +9,28 @@ class AbstractCoreRepository:  # pylint: disable=no-member
     def fetch_one(self, request: str, data_tuple: tuple[Any, ...]) -> Any:
         """Fetch one result from a given request."""
         cursor = self.mysql.cursor(dictionary=True)
-        cursor.execute(request, data_tuple)
-        row = cursor.fetchone()
-
-        if row is None:
-            return None
-
-        hydrated = self.hydrate(row)
-        cursor.close()
-
-        return hydrated
+        try:
+            cursor.execute(request, data_tuple)
+            row = cursor.fetchone()
+            if row is None:
+                return None
+            return self.hydrate(row)
+        finally:
+            cursor.close()
 
     def fetch_multiple(self, request: str, data_tuple: tuple[Any, ...]) -> list[Any]:
         """Fetch mutliple items and return a list."""
         items_list = []
         cursor = self.mysql.cursor(dictionary=True, buffered=True)
-        cursor.execute(request, data_tuple)
-
-        while True:
-            row = cursor.fetchone()
-            if row is None:
-                break
-            items_list.append(self.hydrate(row))
-
-        cursor.close()
+        try:
+            cursor.execute(request, data_tuple)
+            while True:
+                row = cursor.fetchone()
+                if row is None:
+                    break
+                items_list.append(self.hydrate(row))
+        finally:
+            cursor.close()
         return items_list
 
     def hydrate(self, row: dict[str, Any]) -> Any:
@@ -50,14 +48,15 @@ class AbstractCoreRepository:  # pylint: disable=no-member
     def write(self, request: str, data: list[Any] | tuple[Any, ...], commit: bool = True) -> int | None:
         """Performs an UPDATE or WRITE statement"""
         cursor = self.mysql.cursor()
-        cursor.execute(request, data)
-
-        if commit:
-            self.mysql.commit()
-        else:
-            self.mysql.autocommit = False
-
-        return cursor.lastrowid
+        try:
+            cursor.execute(request, data)
+            if commit:
+                self.mysql.commit()
+            else:
+                self.mysql.autocommit = False
+            return cursor.lastrowid
+        finally:
+            cursor.close()
 
     def fetch_cursor(self, request: str, data_tuple: list[Any] | dict[str, Any] | None = None) -> dict[str, Any] | None:
         """Fetch one result"""
@@ -65,8 +64,8 @@ class AbstractCoreRepository:  # pylint: disable=no-member
             data_tuple = {}
 
         cursor = self.mysql.cursor(dictionary=True)
-        cursor.execute(request, data_tuple)
-        row = cursor.fetchone()
-        cursor.close()
-
-        return row
+        try:
+            cursor.execute(request, data_tuple)
+            return cursor.fetchone()
+        finally:
+            cursor.close()
